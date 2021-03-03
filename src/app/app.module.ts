905c7ac37e8be7b482c46d189e9c0c21c1be704d
Luis Carlos <luiscarlos8pe@gmail.com>
2021-03-02 23:05:35 -0300

    Correção componente topo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
 
-import { TopoComponent } from './topo/topo.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+   
     
    
   
--- src/topo/topo.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core'
-
-@Component({
-    selector:'app-topo',
-    templateUrl: './topo.component.html'
-})
-
-export class TopoComponent{}
